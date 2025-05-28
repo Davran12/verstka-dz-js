489df685d9f54d7d850ad6f99666967553f86512
Davran <davranlapa@gmail.com>
2025-05-28 11:40:49 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("openPopup").addEventListener("click", function () {
+  document.getElementById("popup").classList.add("active")
+})
+
+document.getElementById("closePopup").addEventListener("click", function () {
+  document.getElementById("popup").classList.remove("active")
+})
