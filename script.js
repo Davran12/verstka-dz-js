882832a856381dd7adbb93b786f9fb3f1cad4df5
Davran <davranlapa@gmail.com>
2025-06-03 23:15:25 +0600

    swaiper

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ document.getElementById("openPopup").addEventListener("click", function () {
 document.getElementById("closePopup").addEventListener("click", function () {
   document.getElementById("popup").classList.remove("active")
 })
-
-document.getElementById("burger").addEventListener("click", function () {
-  document.getElementById("mobileMenu").classList.add("active")
-})
-
 document.getElementById("closeMobile").addEventListener("click", function () {
   document.getElementById("mobileMenu").classList.remove("active")
 })
@@ -19,4 +14,22 @@ document.getElementById("burger").addEventListener("click", function () {
 
 document.getElementById("closeMobile").addEventListener("click", function () {
   document.getElementById("mobileMenu").classList.remove("active")
+})
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.querySelector(".swiper-wrapper")
+  const card = document.querySelector(".components-card")
+  if (!wrapper || !card) return
+
+  const cardWidth = card.offsetWidth + 20
+
+  const nextBtn = document.querySelector(".swiper-button-next")
+  const prevBtn = document.querySelector(".swiper-button-prev")
+
+  nextBtn.addEventListener("click", () => {
+    wrapper.scrollBy({left: cardWidth, behavior: "smooth"})
+  })
+
+  prevBtn.addEventListener("click", () => {
+    wrapper.scrollBy({left: -cardWidth, behavior: "smooth"})
+  })
 })
